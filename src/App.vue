22d74f6ca0ba0e8b6d10529f73cf4054d826e5e3
yejb <yejb_01@163.com>
2023-12-04 21:58:30 +0800

    初始化项目 1.引入vant 2.引入scss 3.引入lib-flexible postcss-pxtorem

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss">
+.box {
+  width: 100px;
+  height: 100px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import './assets/main.css'
+import 'vant/lib/index.css';
+
+import 'lib-flexible/flexible'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
