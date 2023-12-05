27d4c99e982465324e5b136106c3972154fd5e79
yejb <yejb_01@163.com>
2023-12-05 23:26:12 +0800

    用户接口封装

---
+++ src/api/user.ts
@@ -0,0 +1,38 @@
+// user.js
+import ajax from '@/utils/ajax' // 二次封装的 ajax
+
+type UserInfo = {
+  introduceSign: string
+  loginName: string
+  nickName: string
+}
+
+type LoginParam = {
+  loginName: string
+  passwordMd5: string
+}
+
+type RegisterParam = {
+  loginName: string
+  passwordMd5: string
+}
+
+// 获取用户信息
+export function getUserInfo() {
+  return ajax.get<UserInfo>('/user/info')
+}
+
+// 登录
+export function login(params: LoginParam) {
+  return ajax.post<string>('/user/login', params)
+}
+
+// 登出
+export function logout() {
+  return ajax.post<string>('/user/logout')
+}
+
+// 注册
+export function register(params: RegisterParam) {
+  return ajax.post<string>('/user/register', params)
+}
